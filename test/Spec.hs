239b38223f6af43a3bf8e74f3c5cc8909a1a50ed
Justin Le <justin@jle.im>
2018-11-20 03:18:24 -0800

    starting work on tests, caught some failures already

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
+import           Control.Monad
+import           Data.Foldable
+import           Data.List.NonEmpty (NonEmpty(..))
+import           Data.Map.NonEmpty  (NEMap, Map(..))
+import           Hedgehog
+import           Map
+import           System.Exit
+import           System.IO
+import qualified Data.List.NonEmpty as NE
+import qualified Data.Map           as M
+import qualified Data.Map.NonEmpty  as NEM
+import qualified Data.Set           as S
+import qualified Data.Text          as T
+import qualified Hedgehog.Gen       as Gen
+import qualified Hedgehog.Range     as Range
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    hSetBuffering stdout LineBuffering
+    hSetBuffering stderr LineBuffering
+
+    results <- checkParallel mapTests
+
+    unless results exitFailure
